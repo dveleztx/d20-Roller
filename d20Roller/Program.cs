2effcf53d92ebcfd014c8740e475612f5f6fa783
David <dveleztx@gmail.com>
2018-05-14 20:39:39 -0500

    Got data validation to work, going to convert that to a function for multiple calls

--- d20Roller/Program.cs
+++ d20Roller/Program.cs
@@ -4,12 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace d20Roller
+namespace d20_Roller
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            // Variables
+            int numSides;
+
+            // Random Object
+            Random dice = new Random();
+
+            // User Prompt
+            while (true)
+            {
+                // Input Validation
+                while (true)
+                {
+
+                    try
+                    {
+                        // Choose how many sides the die is
+                        Console.WriteLine("How many sides are on this die? (enter a number)");
+
+                        numSides = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("You must enter a valid number.");
+                        continue;
+                    }
+
+                }
+
+                Console.WriteLine($"Number of sides chosen is: {numSides}");
+
+                Console.ReadKey();
+            }
+
         }
     }
 }
